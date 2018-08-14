7e2a22b8c58c5d761f14e45c4f12d9b94b747f1d
Deepa Ravichandran <deepa5starbee@gmail.com>
2018-08-14 01:13:44 -0400

    Changes to add the logging functionality

--- liri.js
+++ liri.js
@@ -14,7 +14,11 @@ if(process.argv.length < 3 || process.argv.length > 4){
     process.exit();
 }
 
+var commandTxt = "Command: " + process.argv.join(" ");
+writeToLog(commandTxt);
+
 runCommand(process.argv[2], process.argv[3]);
+
 
 /**
  * Function gets the command line arguments and calls the appropriate functions to excute 
@@ -33,7 +37,9 @@ function runCommand(command, commandArg){
             break;
         case "do-what-it-says": readFromFile();
             break;
-        default: console.log("Invalid Parameter: " + command);
+        default: var errmsg = "Invalid Parameter: " + command + "\n";
+        console.log(errmsg);
+        writeToLog(errmsg);
         break;
     }
 }
@@ -45,9 +51,12 @@ function getTweets() {
     var params = { user_id: "Deepa689" };
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error && response.statusCode === 200) {
+            writeToLog("--------------------------------------------")
             for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
+                writeToLog(tweets[i].text);
             }
+            writeToLog("--------------------------------------------")
         }
     });
 }
@@ -66,14 +75,17 @@ function getMovies(movieName) {
 
     Request(requestQueryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("Imdb Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-            console.log("Country: " + JSON.parse(body).Country);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Actors: " + JSON.parse(body).Actors);
-            console.log("Plot: " + JSON.parse(body).Plot);
+            var movieMsg = "Title: " + JSON.parse(body).Title + "\n";
+            movieMsg = movieMsg + "Release Year: " + JSON.parse(body).Year + "\n";
+            movieMsg = movieMsg + "Imdb Rating: " + JSON.parse(body).imdbRating + "\n";
+            movieMsg = movieMsg + "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\n";
+            movieMsg = movieMsg + "Country: " + JSON.parse(body).Country + "\n";
+            movieMsg = movieMsg + "Language: " + JSON.parse(body).Language + "\n";
+            movieMsg = movieMsg + "Actors: " + JSON.parse(body).Actors + "\n";
+            movieMsg = movieMsg + "Plot: " + JSON.parse(body).Plot + "\n";
+
+            writeToLog(movieMsg);
+            console.log(movieMsg);
         }
     });
 }
@@ -88,14 +100,19 @@ function getMusic(trackName) {
     spotify.search({ type: 'track', query: trackName, limit: 10 }, function (err, data) {
 
         if (err) {
-            return console.log('Error occurred: ' + err);
+            writeToLog("Error occured: " + err.message);
+            return console.log('Error occurred: ' + err.message);
+            
         }
 
         for (var i = 0; i < data.tracks.items.length; i++) {
             var albumData = data.tracks.items[i];
 
             console.log("Track Name: " + albumData.name);
+            writeToLog("Track Name: " + albumData.name);
             console.log("Album Name: " + albumData.album.name);
+            writeToLog("Album Name: " + albumData.album.name);
+
             var artistName = "";
             for (var j = 0; j < albumData.artists.length; j++) {
                 if(j != 0)
@@ -106,7 +123,14 @@ function getMusic(trackName) {
             console.log("Release Date: " + albumData.album.release_date);
             console.log("Duration: " + parseInt((albumData.duration_ms / 1000) / 60) + "m " + parseInt((albumData.duration_ms / 1000) % 60) + "s");
             console.log("Preview URL: " + albumData.preview_url);
+
+            writeToLog("Artist: " + artistName);
+            writeToLog("Release Date: " + albumData.album.release_date);
+            writeToLog("Duration: " + parseInt((albumData.duration_ms / 1000) / 60) + "m " + parseInt((albumData.duration_ms / 1000) % 60) + "s");
+            writeToLog("Preview URL: " + albumData.preview_url);
+
             console.log("---------------------------------------------------");
+            writeToLog("---------------------------------------------------");
         }
     });
 }
@@ -124,3 +148,12 @@ function readFromFile() {
     });
 }
 
+function writeToLog(errmsg){
+    errmsg = errmsg.concat("\n");
+    fs.appendFile("log.txt", errmsg, function(err){
+        if(err){
+            console.log(err.message);
+            throw err;
+        }
+    });
+}
